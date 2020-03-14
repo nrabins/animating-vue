78eb9b64341ea4aacc616dfe758c58c49c732f6c
Nathan Rabins <nrabins@gmail.com>
2020-03-14 18:33:13 -0500

    Update to start of lesson 3, page transitions

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Modal from './views/Modal.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Modal',
+      component: Modal
     },
     {
       path: '/about',
