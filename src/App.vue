d58acfec0ab132e45292aae1101b5c936dcbde3c
Nathan Rabins <nrabins@gmail.com>
2020-03-14 21:44:16 -0500

    Add GSAP timeline runners

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,10 @@
       <router-link to="/cards">Cards</router-link> |
       <router-link to="/simple">Simple</router-link> |
       <router-link to="/stagger">Stagger</router-link> |
-      <router-link to="/state">State</router-link>
+      <router-link to="/state">State</router-link> |
+      <router-link to="/timeline">Timeline</router-link>
     </div>
-    <!-- <transition name="slide-fade" mode="out-in"> -->
     <router-view />
-    <!-- </transition> -->
   </div>
 </template>
 
@@ -23,6 +22,7 @@
   text-align: center;
   color: #2c3e50;
 }
+
 button {
   height: 2.2em;
   margin: 0.5em;
@@ -30,6 +30,7 @@ button {
   background: linear-gradient(to right, #16c0b0, #84cf6a);
   color: white;
 }
+
 input {
   display: inline-block;
   box-sizing: border-box;
@@ -39,6 +40,7 @@ input {
   margin: 0.2em;
   font: 0.8em 'Avenir', Helvetica, sans-serif;
 }
+
 .modal {
   width: 250px;
   height: 200px;
@@ -48,55 +50,70 @@ input {
   border-radius: 2%;
   background-color: #e0e0e0;
 }
+
 #nav {
   padding: 30px;
 }
+
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
+
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 p {
   font-size: 0.8em;
 }
+
 ::placeholder {
   color: #2c3e50;
 }
+
 ul {
   list-style-type: none;
 }
+
 /*** TRANSITIONS ***/
 .fade-enter {
   opacity: 0;
 }
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease-out;
 }
+
 .fade-leave-to {
   opacity: 0;
 }
+
 .slide-fade-enter {
   transform: translateX(10px);
   opacity: 0;
 }
+
 .slide-fade-enter-active,
 .slide-fade-leave-active {
   transition: all 0.2s ease;
 }
+
 .slide-fade-leave-to {
   transform: translateX(-10px);
   opacity: 0;
 }
+
 .slide-up-enter {
   transform: translateY(10px);
   opacity: 0;
 }
+
 .slide-up-enter-active {
   transition: all 0.2s ease;
 }
+
 .slide-up-move {
   transition: transform 0.8s ease-in;
 }
